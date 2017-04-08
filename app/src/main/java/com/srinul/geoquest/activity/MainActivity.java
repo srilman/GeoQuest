2085b9a313dd9589e8776c76c43d6a8800263f05
srilman <slade@wpi.edu>
2017-04-08 14:00:48 -0400

    Fragments

--- app/src/main/java/com/srinul/geoquest/activity/MainActivity.java
+++ app/src/main/java/com/srinul/geoquest/activity/MainActivity.java
@@ -1,9 +1,8 @@
-package com.srinul.geoquest;
+package com.srinul.geoquest.activity;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,8 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.srinul.geoquest.R;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -68,21 +69,15 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_map) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new MapFragement()).commit();
+        } else if (id == R.id.nav_puzzles) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new PuzzleFragment()).commit();
+        } else if (id == R.id.nav_settings) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/srinul/geoquest/activity/PuzzleFragment.java
@@ -0,0 +1,23 @@
+package com.srinul.geoquest.activity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.srinul.geoquest.R;
+
+public class PuzzleFragment extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.fragment_puzzle, container, false);
+        return myView;
+    }
+
+}
